db783278f6711dea6445967c79509e139b7c0562
Alexandre Belloni <86206091+Alexandre10y@users.noreply.github.com>
2024-05-24 16:18:56 -0300

    Carregando novo dashboard

--- src/Pages/Project/dashboard.jsx
+++ src/Pages/Project/dashboard.jsx
@@ -1,13 +1,33 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import './style.css'
 function Dashboard() {
     return (
         <>
-            <iframe title="AnaliseDeVendas-V1.0" 
-            className='w-100 height-100' 
-            src="https://app.powerbi.com/view?r=eyJrIjoiNDVjMTIxMDctNWI5OS00NWZlLTgxYzctZTM0N2ZmMmViYjZhIiwidCI6IjBiYWIxNGQ4LTEyNTktNDQyYi05MDQ0LTk2NjU4MWRjOWM2MyJ9" 
-            frameborder="0" 
-            allowFullScreen="true">
-            </iframe>            
+            <Container fluid>
+                <Row>
+                    <Col md={6}>
+                        <iframe
+                            title="AnaliseDeVendas-V1.0"
+                            className='w-100'
+                            style={{ height: '100vh' }} // altura de 100% da viewport
+                            src="https://app.powerbi.com/view?r=eyJrIjoiNDVjMTIxMDctNWI5OS00NWZlLTgxYzctZTM0N2ZmMmViYjZhIiwidCI6IjBiYWIxNGQ4LTEyNTktNDQyYi05MDQ0LTk2NjU4MWRjOWM2MyJ9"
+                            frameBorder="0"
+                            allowFullScreen
+                        />
+                    </Col>
+                    <Col md={6}>
+                        <iframe
+                            title="fCampanhaFacebookAds"
+                            className='w-100'
+                            style={{ height: '100vh' }} // altura de 100% da viewport
+                            src="https://app.powerbi.com/view?r=eyJrIjoiMmMzOGJiZWEtMTdiYy00MzE0LTg4Y2UtMWY0NjVhNzcyMmMwIiwidCI6IjBiYWIxNGQ4LTEyNTktNDQyYi05MDQ0LTk2NjU4MWRjOWM2MyJ9"
+                            frameBorder="0"
+                            allowFullScreen
+                        />
+                    </Col>
+                </Row>
+            </Container>
+
         </>
     );
 }
